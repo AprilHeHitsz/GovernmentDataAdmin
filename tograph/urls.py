1671463a50cb240bf89ce3f763541b4f7353bcdf
april <aprilhehitsz@163.com>
2020-11-30 00:37:01 +0800

    add simple tograph

--- tograph/urls.py
+++ tograph/urls.py
@@ -4,5 +4,9 @@ from . import views
 app_name = 'tograph'
 
 urlpatterns = [
-    url(r'^$', views.show, name='show'),
+    url(r'^$', views.get_graph, name='show'),
+    url(r'^crawl/$', views.crawl, name='crawl'),
+    url(r'^part/$', views.part, name='part'),
+    url(r'^table/$', views.table, name='table'),
+    url(r'^to_graph/$', views.get_graph, name='to_graph'),
 ]
